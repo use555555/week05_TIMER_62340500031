a278da8335a6dd71e2d2729e9671e093fec16183
Pawarich Ngamvuttivorn <42509276+use555555@users.noreply.github.com>
2021-03-25 23:51:55 +0700

    Update2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -150,7 +150,7 @@ int main(void)
 		  CapPulsePerSec = 1000000.0/MeanTime;
 	  }
 
-	  MotorSpeed = (CapPulsePerSec/12)/64;
+	  MotorSpeed = (CapPulsePerSec/12)*60/64;
   }
   /* USER CODE END 3 */
 }
